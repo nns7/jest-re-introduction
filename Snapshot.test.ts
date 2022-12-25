dec8befedcc40dd5a7a97ae4f6cb158bc51ca6d5
nns7 <nns7snn@gmail.com>
2022-12-25 16:43:16 +0900

    :+1: インラインスナップショット

--- Snapshot.test.ts
+++ Snapshot.test.ts
@@ -9,4 +9,23 @@ describe("Snapshot Testing", () => {
     `;
     expect(html).toMatchSnapshot();
   });
+
+  test("toMatchSnapshot - インライン", () => {
+    const html = `
+    <div class="container">
+      <article>
+        <p>UI生成結果<p>
+      </article>
+    </div>
+    `;
+    expect(html).toMatchInlineSnapshot(`
+"
+    <div class="container">
+      <article>
+        <p>UI生成結果<p>
+      </article>
+    </div>
+    "
+`);
+  });
 });
