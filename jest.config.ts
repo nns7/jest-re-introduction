0fc8daa82e9870123c95dfa203acc2a8e58fcca7
nns7 <nns7snn@gmail.com>
2022-12-25 14:52:33 +0900

    :up: 依存パッケージにjest-html-reportersを追加。

--- jest.config.ts
+++ jest.config.ts
@@ -106,7 +106,7 @@ export default {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default", "jest-html-reporters"],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
